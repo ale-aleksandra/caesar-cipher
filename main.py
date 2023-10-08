3574d2c0068244cdee1b992307f08fafcd165e81
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-08 14:53:06 +0100

    enabled user to end or run programme again

--- main.py
+++ main.py
@@ -7,11 +7,6 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
             'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z']
 
-direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
-text = input("Type your message:\n").lower()
-shift = int(input("Type the shift number:\n"))
-shift = shift % 26
-
 
 def caesar(start_text, shift_amount, cipher_direction):
     end_text = ""
@@ -27,4 +22,17 @@ def caesar(start_text, shift_amount, cipher_direction):
     print(f"Here's the {direction}d result: {end_text}")
 
 
-caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+should_end = False
+while not should_end:
+    direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
+    text = input("Type your message:\n").lower()
+    shift = int(input("Type the shift number:\n"))
+    shift = shift % 26
+
+    caesar(start_text=text, shift_amount=shift, cipher_direction=direction)
+
+    restart = input("Type 'yes' if you want to go again. Otherwise type 'no'.\n")
+    if restart == "no":
+        should_end = True
+        print("Goodbye")
+        
