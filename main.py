0d8cd5ee9c52f58f5cf125b61f2baf21c40b2b5f
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-08 14:33:32 +0100

    fixed shift amount bug

--- main.py
+++ main.py
@@ -10,6 +10,7 @@ alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm',
 direction = input("Type 'encode' to encrypt, type 'decode' to decrypt:\n")
 text = input("Type your message:\n").lower()
 shift = int(input("Type the shift number:\n"))
+shift = shift % 26
 
 
 def caesar(start_text, shift_amount, cipher_direction):
