259c1c32fb786dda22b739ab67563c9efdfd94c4
ale-aleksandra <aleksandra99j@gmail.com>
2023-10-08 13:26:00 +0100

    created 'decrypt' function

--- main.py
+++ main.py
@@ -15,8 +15,14 @@ def encrypt(plain_text, shift_amount):
         new_position = position + shift_amount
         new_letter = alphabet[new_position]
         cipher_text += new_letter
-
     print(f"The encoded text is '{cipher_text}'.")
 
 
-encrypt(text, shift)
+def decrypt(cipher_text, shift_amount):
+    plain_text = ""
+    for letter in cipher_text:
+        position = alphabet.index(letter)
+        new_position = position - shift_amount
+        plain_text += alphabet[new_position]
+    print(f"The decoded text is {plain_text}")
+    
